4fa14d0a44c9c816a7671b81b00b291a4e06bca4
mindvision <hello@martinfuruberg.se>
2022-07-04 18:11:13 +0200

    Enable Steam plugin and OSS (not set as active OSS)

--- Source/Ludo/Ludo.Build.cs
+++ Source/Ludo/Ludo.Build.cs
@@ -16,8 +16,14 @@ public class Ludo : ModuleRules
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+		PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+        PrivateIncludePaths.AddRange(
+		   new string[] {
+			  "Ludo"
+			 }
+		);
+    }
 }
